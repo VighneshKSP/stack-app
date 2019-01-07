58edfdb9942a627665667a27d69ef47cda52ad0d
ViKi-KSP <ksp.vighnesh@gmail.com>
2019-01-07 09:39:12 +0530

    feat: Use form for stack configuration
    Signed-off-by: ViKi-KSP <ksp.vighnesh@gmail.com>

--- src/view.js
+++ src/view.js
@@ -3,6 +3,10 @@
  */
 export class View {
     constructor(model) {
+        // Ids of elements used; These can be class variables.
+        this.maxLengthId = 'max-length';
+        this.applyCfgId = 'apply-config';
+    
         this.model = model;
     }
 
@@ -11,7 +15,18 @@ export class View {
 
         container.innerHTML = `
             <div class="values">${this.model.values}</div>
-            <div class="config">${this.model.maxLength}</div>
+
+            <form class="config">
+                <input placeholder="Maximum length of the stack" type="number" required id="${this.maxLengthId}">
+                <input type="button" name="submit" value="Apply" id="${this.applyCfgId}">
+            </form>
         `;
+
+        this.setInitialValues();
+    }
+
+    setInitialValues() {
+        const maxLength = document.getElementById(this.maxLengthId);
+        maxLength.value = this.model.maxLength;
     }
 }
